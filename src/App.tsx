2a36cf8c7ba30938990ad75b4705253f5847f88d
jessicaagren <jessicaagren@hotmail.com>
2025-04-16 10:37:29 +0200

    Add Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,33 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.scss';
+import RootLayout from './layouts/RootLayout';
+import MyBooksPage from './routes/MyBooksPage';
+import BrowsePage from './routes/BrowsePage';
+import SearchPage from './routes/SearchPage';
+import HomePage from './routes/HomePage';
 
-function App() {
-  return (
-    <>
-      <p>The Literary Garden</p>
-    </>
-  );
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout />,
+    children: [
+      { index: true, element: <HomePage /> },
+      {
+        path: 'my-books',
+        element: <MyBooksPage />,
+      },
+      {
+        path: 'browse',
+        element: <BrowsePage />,
+      },
+      {
+        path: 'search',
+        element: <SearchPage />,
+      },
+    ],
+  },
+]);
+
+export default function App() {
+  return <RouterProvider router={router} />;
 }
-
-export default App;
---
+++ src/routes/MyBooksPage.tsx
@@ -0,0 +1,7 @@
+export default function MyBooksPage() {
+  return (
+    <>
+      <div>MyBooks</div>
+    </>
+  );
+}
