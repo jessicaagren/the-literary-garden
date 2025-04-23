3641e9c0320b24245d4522d76079ae741dadffbc
jessicaagren <jessicaagren@hotmail.com>
2025-04-23 12:45:29 +0200

    Add BookCard styling

--- src/components/BookCard/BookCard.tsx
+++ src/components/BookCard/BookCard.tsx
@@ -1,5 +1,13 @@
 import './BookCard.scss';
 
 export default function BookCard() {
-  return <div className='BookCard'>BookCard</div>;
+  return (
+    <div className='BookCard'>
+      <img
+        className='BookCard__image'
+        src='https://upload.wikimedia.org/wikipedia/en/e/e9/First_Single_Volume_Edition_of_The_Lord_of_the_Rings.gif'></img>
+      <p className='BookCard__title'>Title of book</p>
+      <p className='BookCard__author'>Author of book</p>
+    </div>
+  );
 }
