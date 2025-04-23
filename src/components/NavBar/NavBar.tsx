0a416b8bde26aa2a8a1184ee82b838d9a5a18765
jessicaagren <jessicaagren@hotmail.com>
2025-04-23 09:48:24 +0200

    Add styling (with font) to NavBar

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -8,11 +8,20 @@ export default function Navbar() {
       <Link to='/'>
         <h1 className='title2'>The Literary Garden</h1>
       </Link>
-
-      <Link to='/'>Home</Link>
-      <Link to='/my-books'>My Books</Link>
-      <Link to='/browse'>Browse</Link>
-      <Link to='/search'>Search</Link>
+      <div className='NavBarMenu'>
+        <Link to='/' className='Link'>
+          Home
+        </Link>
+        <Link to='/my-books' className='Link'>
+          My Books
+        </Link>
+        <Link to='/browse' className='Link'>
+          Browse
+        </Link>
+        <Link to='/search' className='Link'>
+          Search
+        </Link>
+      </div>
     </nav>
   );
 }
