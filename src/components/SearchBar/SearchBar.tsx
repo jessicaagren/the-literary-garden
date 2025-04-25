1a2fd6a88b41101dde61bedeeb97178b1a5f6b78
jessicaagren <jessicaagren@hotmail.com>
2025-04-25 10:15:27 +0200

    Add basic fetch on search

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -2,31 +2,51 @@ import { useState } from 'react';
 import './SearchBar.scss';
 import { FaSearch } from 'react-icons/fa';
 
-export default function SearchBar() {
-  const [query, setQuery] = useState('');
+export type BookSearchResult = {
+  key: string;
+  author_name: string[];
+  first_publish_year: number;
+  title: string;
+};
 
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-    console.log('Search:', query);
+export default function SearchBar() {
+  const [searchInput, setSearchInput] = useState('');
+  const [searchResults, setSearchResults] = useState<BookSearchResult[]>([]);
+
+  const handleSearch = async () => {
+    if (!searchInput.trim()) return;
+    const data = await fetch(
+      `https://openlibrary.org/search.json?title=${searchInput}`
+    );
+    const jsonData = await data.json();
+    const results = jsonData.docs as BookSearchResult[];
+    setSearchResults(results);
+    console.log(results);
+  };
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      handleSearch();
+    }
   };
 
   return (
-    <div className='SearchBarWrapper'>
-      <form onSubmit={handleSubmit} role='search' className='SearchBar'>
-        <label htmlFor='search'></label>
+    <div className='SearchBar__Wrapper'>
+      <div className='SearchBar'>
         <input
           id='search'
           type='search'
           placeholder='Search...'
           autoFocus
           required
-          value={query}
-          onChange={(e) => setQuery(e.target.value)}
+          value={searchInput}
+          onChange={(e) => setSearchInput(e.target.value)}
+          onKeyDown={handleKeyDown}
         />
-        <button type='submit'>
+        <button onClick={handleSearch} type='button'>
           <FaSearch />
         </button>
-      </form>
+      </div>
     </div>
   );
 }
