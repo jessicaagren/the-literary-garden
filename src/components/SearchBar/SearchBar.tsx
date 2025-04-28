f9a7c4d67c9b501702b7dff68f54495d82d7ccbe
jessicaagren <jessicaagren@hotmail.com>
2025-04-28 15:36:46 +0200

    Add className on icon

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -20,22 +20,19 @@ export default function SearchBar({ onResults }: SearchBarProps) {
 
   const handleSearch = async () => {
     if (!searchInput.trim()) return;
+
     const data = await fetch(
       `https://openlibrary.org/search.json?title=${searchInput}`
     );
     const jsonData = await data.json();
-    const results = (jsonData.docs as any[]).map((doc) => ({
-      key: doc.key,
-      title: doc.title,
-      author_name: doc.author_name || ['Unknown'],
-      first_publish_year: doc.first_publish_year || 'N/A',
-    }));
 
     if (onResults) {
-      onResults(results);
+      onResults(jsonData.docs);
     }
 
-    navigate('/search', { state: { results } });
+    navigate(`/search?query=${searchInput}`, {
+      state: { results: jsonData.docs },
+    });
 
     setSearchInput('');
   };
@@ -45,7 +42,7 @@ export default function SearchBar({ onResults }: SearchBarProps) {
   };
 
   return (
-    <div className='SearchBar__Wrapper'>
+    <div className='SearchBar__wrapper'>
       <div className='SearchBar'>
         <input
           id='search'
@@ -58,7 +55,7 @@ export default function SearchBar({ onResults }: SearchBarProps) {
           onKeyDown={handleKeyDown}
         />
         <button onClick={handleSearch} type='button'>
-          <FaSearch />
+          <FaSearch className='SearchBar__icon' />
         </button>
       </div>
     </div>
