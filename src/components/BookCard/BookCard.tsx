7699f7834d12788e9104ba66ca1a6998b3c76436
jessicaagren <jessicaagren@hotmail.com>
2025-04-28 15:36:11 +0200

    Add Favourites button

--- src/components/BookCard/BookCard.tsx
+++ src/components/BookCard/BookCard.tsx
@@ -1,14 +1,24 @@
+import { useState } from 'react';
+import Button from '../Button/Button';
 import './BookCard.scss';
+import {
+  FaHeartCircleCheck,
+  FaHeartCircleMinus,
+  FaHeartCirclePlus,
+} from 'react-icons/fa6';
 
 interface BookCardProps {
   title: string;
-  author: string[]; // Författare som en array
+  author: string[];
   img?: string;
 }
 
-export default function BookCard({ title, author, img }: BookCardProps) {
-  let formattedAuthors: string;
+export default function BookCard({ title, author = [], img }: BookCardProps) {
+  const [isClicked, setIsClicked] = useState(false);
+  const [isHovered, setIsHovered] = useState(false);
+  const [hasHoverLeftOnce, setHasHoverLeftOnce] = useState(false);
 
+  let formattedAuthors: string;
   if (author.length > 3) {
     formattedAuthors = `${author.slice(0, 3).join(', ')}, etc.`;
   } else if (author.length === 2) {
@@ -17,8 +27,58 @@ export default function BookCard({ title, author, img }: BookCardProps) {
     formattedAuthors = author.join(', ');
   }
 
+  const getIcon = () => {
+    if (!isClicked) {
+      return <FaHeartCirclePlus className='BookCard__icon' />;
+    }
+
+    if (isClicked) {
+      if (!hasHoverLeftOnce) {
+        return (
+          <FaHeartCircleCheck className='BookCard__icon BookCard__icon__check' />
+        );
+      }
+      if (isHovered) {
+        return <FaHeartCircleMinus className='BookCard__icon' />;
+      } else {
+        return (
+          <FaHeartCircleCheck className='BookCard__icon BookCard__icon__check' />
+        );
+      }
+    }
+  };
+
+  const handleClick = () => {
+    setIsClicked((prev) => {
+      const next = !prev;
+      if (!next) {
+        setHasHoverLeftOnce(false);
+      }
+      return next;
+    });
+  };
+
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+
+    if (isClicked && !hasHoverLeftOnce) {
+      setHasHoverLeftOnce(true);
+    }
+  };
+
   return (
     <div className='BookCard'>
+      <Button
+        className='BookCard__icon-wrapper'
+        onClick={handleClick}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}>
+        {getIcon()}
+      </Button>
       <img
         className='BookCard__image'
         src={
