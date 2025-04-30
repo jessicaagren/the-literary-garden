a2e1860ac32f2a8fa0fa033703e653e679768ded
jessicaagren <jessicaagren@hotmail.com>
2025-04-30 11:15:11 +0200

    Add FavouritesButton component

---
+++ src/components/FavouritesButton/FavouritesButton.tsx
@@ -0,0 +1,64 @@
+import { useState } from 'react';
+import {
+  FaHeartCircleCheck,
+  FaHeartCircleMinus,
+  FaHeartCirclePlus,
+} from 'react-icons/fa6';
+import './FavouritesButton.scss';
+
+interface ButtonProps {
+  className?: string;
+}
+
+export default function FavouritesButton({ className = '' }: ButtonProps) {
+  const [isClicked, setIsClicked] = useState(false);
+  const [isHovered, setIsHovered] = useState(false);
+  const [hasHoverLeftOnce, setHasHoverLeftOnce] = useState(false);
+
+  const getIcon = () => {
+    if (!isClicked) {
+      return <FaHeartCirclePlus className='BookCard__icon' />;
+    }
+
+    if (!hasHoverLeftOnce) {
+      return (
+        <FaHeartCircleCheck className='BookCard__icon BookCard__icon__check' />
+      );
+    }
+
+    if (isHovered) {
+      return <FaHeartCircleMinus className='BookCard__icon' />;
+    }
+
+    return (
+      <FaHeartCircleCheck className='BookCard__icon BookCard__icon__check' />
+    );
+  };
+
+  const handleClick = () => {
+    setIsClicked((prev) => {
+      const next = !prev;
+      if (!next) setHasHoverLeftOnce(false);
+      return next;
+    });
+  };
+
+  const handleMouseEnter = () => setIsHovered(true);
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+    if (isClicked && !hasHoverLeftOnce) {
+      setHasHoverLeftOnce(true);
+    }
+  };
+
+  return (
+    <button
+      className={`FavouritesButton ${className}`}
+      onClick={handleClick}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}>
+      {getIcon()}
+    </button>
+  );
+}
