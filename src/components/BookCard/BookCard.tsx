d3916c3a72fd7d8cee6f16c2bf842cc7ba0288db
jessicaagren <jessicaagren@hotmail.com>
2025-04-30 11:15:38 +0200

    Add FavouritesButton component and change styling of BookCards

--- src/components/BookCard/BookCard.tsx
+++ src/components/BookCard/BookCard.tsx
@@ -1,23 +1,13 @@
-import { useState } from 'react';
-import Button from '../Button/Button';
 import './BookCard.scss';
-import {
-  FaHeartCircleCheck,
-  FaHeartCircleMinus,
-  FaHeartCirclePlus,
-} from 'react-icons/fa6';
+import FavouritesButton from '../FavouritesButton/FavouritesButton';
 
 interface BookCardProps {
   title: string;
   author: string[];
-  img?: string;
+  img: string;
 }
 
 export default function BookCard({ title, author = [], img }: BookCardProps) {
-  const [isClicked, setIsClicked] = useState(false);
-  const [isHovered, setIsHovered] = useState(false);
-  const [hasHoverLeftOnce, setHasHoverLeftOnce] = useState(false);
-
   let formattedAuthors: string;
   if (author.length > 3) {
     formattedAuthors = `${author.slice(0, 3).join(', ')}, etc.`;
@@ -27,66 +17,10 @@ export default function BookCard({ title, author = [], img }: BookCardProps) {
     formattedAuthors = author.join(', ');
   }
 
-  const getIcon = () => {
-    if (!isClicked) {
-      return <FaHeartCirclePlus className='BookCard__icon' />;
-    }
-
-    if (isClicked) {
-      if (!hasHoverLeftOnce) {
-        return (
-          <FaHeartCircleCheck className='BookCard__icon BookCard__icon__check' />
-        );
-      }
-      if (isHovered) {
-        return <FaHeartCircleMinus className='BookCard__icon' />;
-      } else {
-        return (
-          <FaHeartCircleCheck className='BookCard__icon BookCard__icon__check' />
-        );
-      }
-    }
-  };
-
-  const handleClick = () => {
-    setIsClicked((prev) => {
-      const next = !prev;
-      if (!next) {
-        setHasHoverLeftOnce(false);
-      }
-      return next;
-    });
-  };
-
-  const handleMouseEnter = () => {
-    setIsHovered(true);
-  };
-
-  const handleMouseLeave = () => {
-    setIsHovered(false);
-
-    if (isClicked && !hasHoverLeftOnce) {
-      setHasHoverLeftOnce(true);
-    }
-  };
-
   return (
     <div className='BookCard'>
-      <Button
-        className='BookCard__icon-wrapper'
-        onClick={handleClick}
-        onMouseEnter={handleMouseEnter}
-        onMouseLeave={handleMouseLeave}>
-        {getIcon()}
-      </Button>
-      <img
-        className='BookCard__image'
-        src={
-          img ||
-          'https://upload.wikimedia.org/wikipedia/en/e/e9/First_Single_Volume_Edition_of_The_Lord_of_the_Rings.gif'
-        }
-        alt={title}
-      />
+      <FavouritesButton className='BookCard__icon-wrapper' />
+      <img className='BookCard__image' src={img} alt={title} />
       <p className='BookCard__title'>{title}</p>
       <p className='BookCard__author'>{formattedAuthors}</p>
     </div>
