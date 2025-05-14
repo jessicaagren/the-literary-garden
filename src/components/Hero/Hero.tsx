cc31eb3400b7fb0071f2e5574b8f62265abdda03
jessicaagren <jessicaagren@hotmail.com>
2025-05-14 21:38:13 +0200

    Add classname to Button for styling

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -4,7 +4,7 @@ import './Hero.scss';
 export default function Hero() {
   return (
     <div className='Hero'>
-      <Button title='Browse' to='/browse' />
+      <Button title='Browse' to='/browse' className='Button__HomePage' />
     </div>
   );
 }
