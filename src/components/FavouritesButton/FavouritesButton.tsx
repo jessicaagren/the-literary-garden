f5be16633a755e2d60748c329b2069bf9d614193
jessicaagren <jessicaagren@hotmail.com>
2025-05-14 21:36:02 +0200

    Cleanup

--- src/components/FavouritesButton/FavouritesButton.tsx
+++ src/components/FavouritesButton/FavouritesButton.tsx
@@ -1,32 +1,57 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import {
   FaHeartCircleCheck,
   FaHeartCircleMinus,
   FaHeartCirclePlus,
 } from 'react-icons/fa6';
+import { useBooksContext } from '../../contexts/BooksContext';
+import { BookType } from '../../types/BookType';
+
 import './FavouritesButton.scss';
 
-interface ButtonProps {
+interface FavouritesButtonProps {
+  book: BookType;
   className?: string;
 }
 
-export default function FavouritesButton({ className = '' }: ButtonProps) {
-  const [isClicked, setIsClicked] = useState(false);
+export default function FavouritesButton({
+  book,
+  className = '',
+}: FavouritesButtonProps) {
+  const { state, dispatch } = useBooksContext();
+  const location = useLocation();
+
   const [isHovered, setIsHovered] = useState(false);
-  const [hasHoverLeftOnce, setHasHoverLeftOnce] = useState(false);
+  const [hasLeftOnce, setHasLeftOnce] = useState(false);
+
+  const isFavourite = state.favourites.some((fav) => fav.key === book.key);
+  const isOnFavouritesPage = location.pathname.startsWith('/my-books');
+
+  useEffect(() => {
+    if (!isOnFavouritesPage) {
+      setHasLeftOnce(false);
+    }
+  }, [isOnFavouritesPage]);
+
+  const handleClick = () => {
+    if (isFavourite) {
+      dispatch({ type: 'REMOVE_FAVOURITE', payload: book.key });
+    } else {
+      dispatch({ type: 'ADD_FAVOURITE', payload: book });
+
+      if (!isOnFavouritesPage) {
+        setHasLeftOnce(false);
+      }
+    }
+  };
 
   const getIcon = () => {
-    if (!isClicked) {
+    if (!isFavourite) {
       return <FaHeartCirclePlus className='FavouritesButton__icon' />;
     }
 
-    if (!hasHoverLeftOnce) {
-      return (
-        <FaHeartCircleCheck className='FavouritesButton__icon FavouritesButton__icon__check' />
-      );
-    }
-
-    if (isHovered) {
+    if (isHovered && (isOnFavouritesPage || hasLeftOnce)) {
       return <FaHeartCircleMinus className='FavouritesButton__icon' />;
     }
 
@@ -35,29 +60,23 @@ export default function FavouritesButton({ className = '' }: ButtonProps) {
     );
   };
 
-  const handleClick = () => {
-    setIsClicked((prev) => {
-      const next = !prev;
-      if (!next) setHasHoverLeftOnce(false);
-      return next;
-    });
-  };
-
-  const handleMouseEnter = () => setIsHovered(true);
-
-  const handleMouseLeave = () => {
-    setIsHovered(false);
-    if (isClicked && !hasHoverLeftOnce) {
-      setHasHoverLeftOnce(true);
-    }
-  };
-
   return (
     <button
-      className={`FavouritesButton ${isClicked ? 'clicked' : ''} ${className}`}
-      onClick={handleClick}
-      onMouseEnter={handleMouseEnter}
-      onMouseLeave={handleMouseLeave}>
+      className={`FavouritesButton ${
+        isFavourite ? 'clicked' : ''
+      } ${className}`}
+      onClick={(e) => {
+        e.preventDefault();
+        e.stopPropagation();
+        handleClick();
+      }}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => {
+        setIsHovered(false);
+        if (!isOnFavouritesPage) {
+          setHasLeftOnce(true);
+        }
+      }}>
       {getIcon()}
     </button>
   );
