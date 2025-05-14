fa4fd014db61b422180b451fdbe0db7b29c8bc68
jessicaagren <jessicaagren@hotmail.com>
2025-05-14 21:38:37 +0200

    Cleanup component and structure

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,24 +1,26 @@
 import { NavLink } from 'react-router-dom';
-import './NavBar.scss';
 import SearchBar from '../SearchBar/SearchBar';
-import FlowerComponent from '../FlowerComponent/FlowerComponent';
+import FlowerIcon from '../FlowerIcon/FlowerIcon';
+import './NavBar.scss';
 
 export default function Navbar() {
+  const getLinkClass = ({ isActive }: { isActive: boolean }) =>
+    `Link${isActive ? ' active' : ''}`;
+
   return (
     <nav className='NavBar'>
       <NavLink to='/' className='NavBar__title_wrapper'>
-        <FlowerComponent />
+        <FlowerIcon />
         <h1 className='NavBar__title'>The Literary Garden</h1>
       </NavLink>
       <div className='NavBar__menu'>
-        <NavLink
-          to='/my-books'
-          className={({ isActive }) => `Link ${isActive ? 'active' : ''}`}>
+        <NavLink to='/my-books' className={getLinkClass}>
           My Books
         </NavLink>
-        <NavLink
-          to='/browse'
-          className={({ isActive }) => `Link ${isActive ? 'active' : ''}`}>
+        <NavLink to='/my-stats' className={getLinkClass}>
+          Stats
+        </NavLink>
+        <NavLink to='/browse' className={getLinkClass}>
           Browse
         </NavLink>
         <SearchBar />
