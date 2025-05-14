cb83b06284711ce1403a99467fefe126e36a4501
jessicaagren <jessicaagren@hotmail.com>
2025-05-14 21:37:54 +0200

    Add SocialsIcons to Footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,17 @@
 import './Footer.scss';
+import { BsLinkedin, BsGithub } from 'react-icons/bs';
 
 export default function Footer() {
-  return <footer className='Footer'>Footer</footer>;
+  return (
+    <footer className='Footer'>
+      <div className='SocialsIcons'>
+        <a href='https://www.linkedin.com/in/jessicaagren/' target='_blank'>
+          <BsLinkedin />
+        </a>
+        <a href='https://github.com/jessicaagren' target='_blank'>
+          <BsGithub />
+        </a>
+      </div>
+    </footer>
+  );
 }
