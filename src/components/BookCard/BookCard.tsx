72772d1a7053d74028f58ff8f4b8bfa7726d528e
jessicaagren <jessicaagren@hotmail.com>
2025-05-14 21:36:59 +0200

    Add imagewrapper class and add img

--- src/components/BookCard/BookCard.tsx
+++ src/components/BookCard/BookCard.tsx
@@ -1,39 +1,46 @@
 import './BookCard.scss';
-import FavouritesButton from '../FavouritesButton/FavouritesButton';
 import { Link } from 'react-router-dom';
+import FavouritesButton from '../FavouritesButton/FavouritesButton';
+import { BookType } from '../../types/BookType';
+import { BookEditionType } from '../../types/BookEditionType';
 
 interface BookCardProps {
-  title: string;
-  author: string[];
-  img: string;
-  bookId: string;
+  book: BookType;
+  edition?: BookEditionType;
 }
 
-export default function BookCard({
-  title,
-  author = [],
-  img,
-  bookId,
-}: BookCardProps) {
-  let formattedAuthors: string;
-  if (author.length > 3) {
-    formattedAuthors = `${author.slice(0, 3).join(', ')}, etc.`;
-  } else if (author.length === 2) {
-    formattedAuthors = `${author[0]} & ${author[1]}`;
-  } else {
-    formattedAuthors = author.join(', ');
-  }
+export default function BookCard({ book }: BookCardProps) {
+  const title = book.title || 'Untitled';
+
+  const authors =
+    book.authors?.map((a: any) => a.name || a.author?.name || 'Unknown') || [];
+
+  const formattedAuthors = (() => {
+    if (authors.length > 3) return `${authors.slice(0, 3).join(', ')}, etc.`;
+    if (authors.length === 2) return `${authors[0]} & ${authors[1]}`;
+    return authors.join(', ') || 'Unknown author';
+  })();
+
+  const bookId = book.key?.replace('/works/', '') || '';
+  const coverId = (book.cover_i || book.covers?.[0]) as number | undefined;
+  const img = coverId
+    ? `https://covers.openlibrary.org/b/id/${coverId}-M.jpg`
+    : 'https://placehold.co/200x300?text=Cover+unavailable';
 
   return (
-    <>
-      <FavouritesButton />
-      <Link to={`/search/${bookId}`} className='BookCard__link'>
-        <div className='BookCard'>
-          <img className='BookCard__image' src={img} alt={title} />
-          <p className='BookCard__title'>{title}</p>
-          <p className='BookCard__author'>{formattedAuthors}</p>
+    <Link to={`/books/${bookId}`} className='BookCard__link'>
+      <div className='BookCard'>
+        <div className='BookCard__imageWrapper'>
+          <FavouritesButton book={{ key: book.key }} />
+          <img
+            className='BookCard__image'
+            src={img}
+            alt={`Cover for ${title}`}
+          />
         </div>
-      </Link>
-    </>
+        <p className='BookCard__title'>{title}</p>
+        <p className='BookCard__author'>{formattedAuthors}</p>
+      </div>
+    </Link>
   );
 }
